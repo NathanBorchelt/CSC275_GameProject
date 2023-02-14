6b95b78798f9baf68398f1d41bdb6e4718568192
ksjak7 <alecrwatkins@gmail.com>
2023-02-14 12:49:09 -0600

    not complete

--- game.py
+++ game.py
@@ -1,8 +1,6 @@
 import pygame
-import random
-import MenuEntities
-import Player
-import math
+import time
+from Player import *
 
 WIDTH = 1600
 HEIGHT = 900
@@ -19,61 +17,18 @@ BLUE = (0,0,255)
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Joypack Jetride")
 clock = pygame.time.Clock()
 #create sprite group and add the player sprite to it
 all_sprites = pygame.sprite.Group()
-menuCursor = MenuEntities.MenuCursor() 
-startButton = MenuEntities.MenuButton(HEIGHT/2, "res/startText.png")
-endButton = MenuEntities.MenuButton(HEIGHT/2+80, "res/endText.png")
-
-all_sprites.add(menuCursor)
-all_sprites.add(startButton)
-all_sprites.add(endButton)
-prevCursorPos = [Player.Vector2f(750, 750)]
-
-
-#Game loop
+#Game loop``
 running = True
-game = False
-menu = True
-follow = False
-deleteEnd = False
-limitor = 150
-x = 0
-
+game = True
+player = Player()
+all_sprites.add(player)
+keyDownSpace = False
+startTime = time.time()
 while running:
-    while menu:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                menu = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-                    menu = False
-                if event.key == pygame.K_w:
-                    menuCursor.rect.y -= 80
-                if event.key == pygame.K_s:
-                    menuCursor.rect.y += 80
-                if event.key == pygame.K_SPACE:
-                    if menuCursor.rect.y == startButton.rect.y:
-                        menu = False
-                        game = True
-                        all_sprites.empty()
-                        player = Player.Player()
-                        cursor = Player.Cursor()
-                        all_sprites.add(player)
-                        all_sprites.add(cursor)
-                    if menuCursor.rect.y == endButton.rect.y:
-                        running = False
-                        menu = False
-        screen.fill(BLACK)
-        all_sprites.update()       
-       
-        all_sprites.draw(screen)
-        pygame.display.flip()
     while game:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -82,27 +37,19 @@ while running:
                 running = False
                 game = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    cursor.pos.y -= 100
-                elif event.key == pygame.K_a:
-                    cursor.pos.x -= 100
-                elif event.key == pygame.K_s:
-                    cursor.pos.y += 100
-                elif event.key == pygame.K_d:
-                    cursor.pos.x += 100 
-                elif event.key == pygame.K_SPACE:
-                    follow = True
-                elif event.key == pygame.K_RIGHT:
-                    limitor += 100
-                    print("hi")
-                elif event.key == pygame.K_LEFT:
-                    limitor -= 100
+                if event.key == pygame.K_SPACE:
+                    keyDownSpace = True
                 elif event.key == pygame.K_ESCAPE:
                     running = False
                     game = False
-
-        #prevents list from being 0 and stops the player follow loop
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    keyDownSpace = False
+                    startTime = time.time()
         
+        if keyDownSpace:
+
+            player.rect.y -= 15
 
         all_sprites.update()       
         screen.fill(BLACK)
