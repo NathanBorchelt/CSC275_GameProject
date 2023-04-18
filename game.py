be9b73ccee65601ac6c72ba268f79605a79f0c80
Nathan Borchelt <nathan.d.borchelt@gmail.com>
2023-04-18 13:45:59 -0500

    push game class

--- game.py
+++ game.py
@@ -2,106 +2,148 @@ import pygame
 import time
 from Player import *
 
-        
-# initialize pygame and create window
-pygame.init()
-pygame.mixer.init()  
-screen = pygame.display.set_mode((st.SCREEN_WIDTH, st.SCREEN_HEIGHT))
-pygame.display.set_caption("Joypack Jetride")
-clock = pygame.time.Clock()
-#create sprite group and add the player sprite to it
-all_sprites = pygame.sprite.Group()
-ground_sprites = pygame.sprite.Group()
-lasers = pygame.sprite.Group()
-obstacles = pygame.sprite.Group()
-warnings = pygame.sprite.Group()
-missles = pygame.sprite.Group()
-bullets = pygame.sprite.Group()
+
+
+class Game:
+        def __init__(self):
+            # initialize pygame and create window
+            pygame.init()
+            pygame.mixer.init()
+            self.screen = pygame.display.set_mode((st.SCREEN_WIDTH,st.SCREEN_HEIGHT))
+            pygame.display.set_caption("Joypack Jetride")
+            self.clock = pygame.time.Clock()
+
+            #all sprite groups
+
+
+            #menu logic
+            self.running = True
+            self.game = False
+            self.menu = True
+
+            #movement binaries
+            self.keyPressedSpace = False
+            self.keyPressedUp = False
+            self.keyPressedLeft = False
+            self.keyPressedDown = False
+            self.keyPressedRight = False
+
+        def load(self):
+            with open("highscore.txt", "r") as file:
+                try:
+                    self.highscore = int(file.readline())
+                except:
+                    self.highscore = 0
+
+        def new(self):
+            self.all_sprites = pygame.sprite.Group()
+            self.ground_sprites = pygame.sprite.Group()
+            self.lasers = pygame.sprite.Group()
+            self.obstacles = pygame.sprite.Group()
+            self.warnings = pygame.sprite.Group()
+            self.missles = pygame.sprite.Group()
+            self.bullets = pygame.sprite.Group()
+            self.player = Player()
+            self.haz = Hazard(True,0)
+
+            self.all_sprites.add(haz)
+
+            self.all_sprites.add(player)
+            self.obstacles.add(haz)
+            self.lasers.add(haz)
+            self.obstacles.add(haz.head)
+            self.obstacles.add(haz.tail)
+            self.all_sprites.add(haz.head)
+            self.all_sprites.add(haz.tail)
+
 #Game loop
-running = True
-game = False
-menu = True
-keyPressedSpace = False
-keyPressedUp = False
-keyPressedLeft = False
-keyPressedDown = False
-keyPressedRight = False
+            self.timeCounter = 0
+            self.font = pygame.font.Font('res/New Athletic M54.ttf', 36)
+            self.score = 0
+            self.frameCounter = 0
+            self.randomHazardSpawnTime = 7
 
-player = Player()
-haz = Hazard(True,0)
 
-all_sprites.add(haz)
 
-all_sprites.add(player)
-obstacles.add(haz)
-lasers.add(haz)
-obstacles.add(haz.head)
-obstacles.add(haz.tail)
-all_sprites.add(haz.head)
-all_sprites.add(haz.tail)
-timeCounter = 0
-font = pygame.font.Font('res/New Athletic M54.ttf', 36)
-score = 0
-frameCounter = 0
-randomHazardSpawnTime = 7
+            self.startGame = pygame.image.load("res/menu/menuFrame.png").convert_alpha()
+            self.startGame = pygame.transform.scale_by(self.startGame, st.scaleFactor)
+            self.cursor = Cursor(st.SCREEN_WIDTH/2, st.SCREEN_HEIGHT/2)
+            self.cursorGroup = pygame.sprite.Group()
+            self.cursorGroup.add(self.cursor)
 
-with open("highscore.txt", "r") as file:
-    try:
-        highscore = int(file.readline())
-    except:
-        highscore = 0
+            self.menu = True
+            self.running = True
+            self.game = False
 
-startGame = pygame.image.load("res/menu/menuFrame.png").convert_alpha()
-startGame = pygame.transform.scale_by(startGame, st.scaleFactor)
-cursor = Cursor(st.SCREEN_WIDTH/2, st.SCREEN_HEIGHT/2)
-cursorGroup = pygame.sprite.Group()
-cursorGroup.add(cursor)
+        def execution(self):
+            while self.running:
+                while self.menu:
+                    self.menu = self.menuLoop()
+                while self.game:
+                    self.game = self.run()
+
+        def menuLoop(self):
+            for event in pygame.event.get():
+                # check for closing window
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    return False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE or event.key == pygame.K_RETURN:
+                        self.keyPressedSpace = True
+                    if event.key == pygame.K_ESCAPE:
+                        self.running = False
+                        return False
+                    if event.key == pygame.K_UP:
+                        self.cursor.rect.y -= 1.5 * self.startGame.get_height()
+                    if event.key == pygame.K_DOWN:
+                        self.cursor.rect.y += 1.5 * self.startGame.get_height()
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_SPACE or event.key == pygame.K_RETURN:
+                        self.keyPressedSpace = False
+
+            if self.cursor.rect.centery < st.SCREEN_HEIGHT/2:
+                self.cursor.rect.centery = st.SCREEN_HEIGHT/2
+            elif self.cursor.rect.centery > st.SCREEN_HEIGHT/2 + 3 * self.startGame.get_height():
+                self.cursor.rect.centery = st.SCREEN_HEIGHT/2 + 3 * self.startGame.get_height()
+
+            if self.keyPressedSpace:
+                if self.cursor.rect.centery <= st.SCREEN_HEIGHT/2:
+                    self.startTime = time.time()
+                    self.missleStartTime = time.time()
+                    self.missleInterval = randint(3, 7)
+                    self.game = True
+                    return False
+
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(self.startGame, (st.SCREEN_WIDTH/2 - self.startGame.get_width()/2, st.SCREEN_HEIGHT/2 - self.startGame.get_height()/2))
+            self.screen.blit(self.startGame, (st.SCREEN_WIDTH/2 - self.startGame.get_width()/2, st.SCREEN_HEIGHT/2 + self.startGame.get_height()))
+            self.screen.blit(self.startGame, (st.SCREEN_WIDTH/2 - self.startGame.get_width()/2, st.SCREEN_HEIGHT/2 + self.startGame.get_height()* 2.5))
+            self.cursorGroup.draw(self.screen)
+            pygame.display.flip()
+            return True
+
+        def run(self):
+            self.timeSinceStart = time.time() - startTime
+            self.player.speedUpdate(1)
+
+            if self.timeSinceStart > self.randomHazardSpawnTime:
+                self.startTime = time.time()
+            self.randomHazardSpawnTime = randint(5, 10)
+            match(randint(0, 3)):
+                case _:
+                    tempHaz = Gun([self.all_sprites, self.obstacles])
+                    self.all_sprites.add(tempHaz)
+                    self.obstacles.add(tempHaz)
+            self.clock.tick(st.FPS)
 
 while running:
-    while menu: 
-        for event in pygame.event.get():
-            # check for closing window
-            if event.type == pygame.QUIT:
-                running = False
-                menu = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE or event.key == pygame.K_RETURN:
-                    keyPressedSpace = True
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-                    menu = False
-                if event.key == pygame.K_UP:
-                    cursor.rect.y -= 1.5 * startGame.get_height()
-                if event.key == pygame.K_DOWN:
-                    cursor.rect.y += 1.5 * startGame.get_height()
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_SPACE or event.key == pygame.K_RETURN:
-                    keyPressedSpace = False  
-            
-        if cursor.rect.centery < st.SCREEN_HEIGHT/2:
-            cursor.rect.centery = st.SCREEN_HEIGHT/2
-        elif cursor.rect.centery > st.SCREEN_HEIGHT/2 + 3 * startGame.get_height():
-            cursor.rect.centery = st.SCREEN_HEIGHT/2 + 3 * startGame.get_height()
 
-        if keyPressedSpace:
-            if cursor.rect.centery <= st.SCREEN_HEIGHT/2:
-                menu = False
-                game = True
-                startTime = time.time()
-                missleStartTime = time.time()
-                missleInterval = randint(3, 7)
-            
-        screen.fill((0, 0, 0))
-        screen.blit(startGame, (st.SCREEN_WIDTH/2 - startGame.get_width()/2, st.SCREEN_HEIGHT/2 - startGame.get_height()/2))
-        screen.blit(startGame, (st.SCREEN_WIDTH/2 - startGame.get_width()/2, st.SCREEN_HEIGHT/2 + startGame.get_height()))
-        screen.blit(startGame, (st.SCREEN_WIDTH/2 - startGame.get_width()/2, st.SCREEN_HEIGHT/2 + startGame.get_height()* 2.5))
-        cursorGroup.draw(screen)
-        pygame.display.flip()
 
     while game:
         timeSinceStart = time.time() - startTime
         player.speedUpdate(1)
-        
+
         if timeSinceStart > randomHazardSpawnTime:
             startTime = time.time()
             randomHazardSpawnTime = randint(5, 10)
@@ -140,7 +182,7 @@ while running:
                             bullet = Bullet(player.rect.centerx + player.rect.width/2, player.rect.centery)
                             all_sprites.add(bullet)
                             bullets.add(bullet)
-                        keyPressedSpace = False       
+                        keyPressedSpace = False
                     case pygame.K_RETURN:
                         keyPressedSpace = False
                     case pygame.K_RIGHT:
@@ -156,8 +198,8 @@ while running:
             player.vely += st.PLAYER_ACC
             if player.vely > 15:
                 player.vely = 15
-            
-            player.flying = True           
+
+            player.flying = True
         elif keyPressedDown:
             player.vely -= st.PLAYER_ACC
             if player.vely < -15:
@@ -198,13 +240,13 @@ while running:
             warnings.add(warning)
 
         for warning in warnings:
-            if warning.spawn > 3: 
+            if warning.spawn > 3:
                 missle = Missle(warning.rect.y)
                 obstacles.add(missle)
                 missles.add(missle)
                 all_sprites.add(missle)
                 warning.kill()
-        
+
         for i in obstacles:
             if pygame.sprite.collide_mask(player, i):
                     running = False
@@ -227,7 +269,7 @@ while running:
 
         if int(score/300) > highscore:
             highscore = int(score/300)
-            
+
         for i in lasers:
             spawnPlatform = i.rect.centerx < st.SCREEN_WIDTH/2
             if spawnPlatform and i.mother:
@@ -240,8 +282,8 @@ while running:
                 obstacles.add(haz2.tail)
                 all_sprites.add(haz2.head)
                 all_sprites.add(haz2.tail)
-                if (bool(randint(0,1))):    
-                    haz3 = Hazard(False, randint(200 , round(st.SCREEN_WIDTH/4 )*2))   
+                if (bool(randint(0,1))):
+                    haz3 = Hazard(False, randint(200 , round(st.SCREEN_WIDTH/4 )*2))
                     lasers.add(haz3)
                     obstacles.add(haz3)
                     all_sprites.add(haz3)
@@ -249,11 +291,11 @@ while running:
                     obstacles.add(haz3.tail)
                     all_sprites.add(haz3.head)
                     all_sprites.add(haz3.tail)
-        
+
         text_surface = font.render(str(int(score/300)) + " M", True, (0,0,0))
         highscore_surface = font.render("Highscore " + str(highscore) + " M", True, (0, 0, 0))
         score += st.playerSpeed
-        all_sprites.update() 
+        all_sprites.update()
         screen.fill((50, 50, 200))
         all_sprites.draw(screen)
         screen.blit(text_surface, (25, 25))
